0c140c82deb55d85776b342521ca79c1602544d4
jakub <jakubdudrak21@hotmail.com>
2022-02-09 10:27:52 +0000

    Lab Sheet 1

--- src/Robot.java
+++ src/Robot.java
@@ -1,11 +1,43 @@
+import static java.lang.Thread.sleep;
+
 public class Robot {
+
     private String name;
+    private boolean working = false;
 
     public Robot(String name) {
-        this.name = name;
+        setName(name);
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name){
+        if(name.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    public boolean isWorking(){
+        return working;
+    }
+
+    public void talkToRobot(){
+        this.working = true;
+    }
+
+    public String getWorkingMsg(){
+        if(!working){
+            throw new IllegalStateException();
+        }
+        return "I am in working mode";
+    }
+
+    public void waitTillWorking() throws InterruptedException{
+        //while(!working){
+            sleep(100);
+        //}
     }
 }
---
+++ test/RobotTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobotTest {
+    Robot buddy = new Robot("Buddy");
+
+    @Test
+    public void getName(){
+        assertEquals("Buddy", buddy.getName());
+    }
+
+    @Test
+    public void isNotWorking(){
+        assertFalse(buddy.isWorking());
+    }
+
+    @Test
+    public void isWorking(){
+        buddy.talkToRobot();
+        assertTrue(buddy.isWorking());
+    }
+
+    @Test
+    public void getWorkingMsg(){
+        buddy.talkToRobot();
+        assertEquals("I am in working mode", buddy.getWorkingMsg());
+    }
+
+    @Test
+    @DisplayName("getWorkingMsgFail catch the exception")
+    public void getWorkingMsgFail(){
+        assertThrows(IllegalStateException.class, () ->{
+            buddy.getWorkingMsg();}
+        );
+    }
+
+    @Test
+    public void testName_Fail(){
+        assertThrows(IllegalArgumentException.class, () ->{
+        Robot chuck = new Robot("");}
+        );
+    }
+
+    @Test
+    public void waitTillWorking() throws InterruptedException {
+        buddy.waitTillWorking();
+    }
+
+    @Test void timeoutExceeded() {
+        assertTimeout(ofMillis(10), () -> {
+            buddy.waitTillWorking(); }
+        );
+    }
+}
