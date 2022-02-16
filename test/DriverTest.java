9fe88c64c41f708c5348db9c08b8228c2f43dab8
jakub <jakidaboii@gmail.com>
2022-02-16 07:03:17 +0000

    V 1.0.0

--- test/DriverTest.java
+++ test/DriverTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DriverTest {
@@ -9,5 +11,20 @@ class DriverTest {
     public void getDriverName(){
         assertEquals("John", john.getDriverName());
     }
+    @Test
+    public void check_status(){
+        assertEquals(false, john.check_status());
+    }
 
+    @Test
+    public void check_status_fail(){
+        assertEquals(true,john.check_status());
+    }
+
+    @Test
+    public void timeoutExceeded(){
+        assertTimeout(Duration.ofMillis(100), () -> {
+            john.waitTillbanned();
+        });
+    }
 }
